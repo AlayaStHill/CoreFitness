c9d8aa4e0df5fb08645c1e75aa863d640385e26b
AlayaStHill <alaya.st.hill@gmail.com>
2026-03-21 12:26:18 +0100

    feat(customer-service): Add contact form markup and ContactForm model.

--- CoreFitness.Presentation.WebApp/Controllers/CustomerServiceController.cs
+++ CoreFitness.Presentation.WebApp/Controllers/CustomerServiceController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreFitness.Presentation.WebApp.Models.Forms;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreFitness.Presentation.WebApp.Controllers;
 
+
 public class CustomerServiceController : Controller
 {
+    [HttpGet]
     public IActionResult Index()
     {
         return View();
     }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Index(ContactForm form)
+    {
+        return View();
+    }
+
 }
+
+
+
