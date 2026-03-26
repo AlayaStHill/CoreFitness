da9dd38c443775ecf5cffa250230dd730f44acb7
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-03-26 11:39:09 +0100

    feat(Domain): add Booking model

---
+++ CoreFitness.Domain/Aggregates/Bookings/Booking.cs
@@ -0,0 +1,23 @@
+﻿using corefitness.domain.shared.validators;
+using CoreFitness.Domain.Aggregates.Workouts.WorkoutSessions;
+
+namespace CoreFitness.Domain.Aggregates.Bookings;
+
+public sealed class Booking
+{
+    public WorkoutSessionId WorkoutSessionId { get; private set; } = default!;
+    public string UserId { get; private set; } = null!;
+    private Booking(WorkoutSessionId workoutSessionId, string userId)
+    {
+        WorkoutSessionId = workoutSessionId;
+        UserId = userId;
+    }
+    private Booking() { }
+    public static Booking Create(WorkoutSessionId workoutSessionId, string userId)
+    {
+        DomainValidator.RequiredGuid(workoutSessionId.Value, BookingErrors.WorkoutSessionIdRequired);
+        DomainValidator.RequiredString(userId, BookingErrors.UserIdRequired);
+
+        return new(workoutSessionId, userId);
+    }
+}
---
+++ CoreFitness.Domain/Aggregates/Bookings/BookingErrors.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreFitness.Domain.Aggregates.Bookings;
+
+internal class BookingErrors
+{
+    public const string WorkoutSessionIdRequired = "WorkoutSessionId is required.";
+
+    public const string UserIdRequired = "UserId is required.";
+}
