c9976a01ea2c7882868ab63f4a33d97c5d36c3bc
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-03-26 11:53:16 +0100

    feat(Domain): add MembershipType model

---
+++ CoreFitness.Domain/Aggregates/Members/MembershipTypes/MembershipType.cs
@@ -0,0 +1,29 @@
+﻿using corefitness.domain.shared.validators;
+using CoreFitness.Domain.Exceptions.Custom;
+
+namespace CoreFitness.Domain.Aggregates.Members.MembershipTypes;
+
+public sealed class MembershipType
+{
+    public MembershipTypeId Id { get; private set; } = default!;
+    public string Name { get; private set; } = null!;
+    public decimal PricePerMonth { get; private set; }
+    private MembershipType(MembershipTypeId id, string name, decimal price)
+    {
+        Id = id;
+        Name = name;
+        PricePerMonth = price;
+    }
+    private MembershipType() { }
+    public static MembershipType Create(string name, decimal price)
+    {
+        DomainValidator.RequiredString(name, MembershipTypeErrors.NameRequired);
+
+        if (price < 0)
+        {
+            throw new ValidationDomainException(MembershipTypeErrors.PriceMustBePositive);
+        }
+
+        return new(MembershipTypeId.Create(), name, price);
+    }
+}
