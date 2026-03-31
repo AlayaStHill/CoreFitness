8aed8b202767cf4518f8f5cc3d71e93c93fd0135
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-03-31 12:43:46 +0200

    add ClassesPerMonth and Description prop to MembershipType. add initialize data for membershipType

---
+++ CoreFitness.Infrastructure/Persistence/EfCore/Migrations/20260331103705_AddDescriptionAndClassesPerMonthToMembershipType.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoreFitness.Infrastructure.Persistence.EfCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionAndClassesPerMonthToMembershipType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClassesPerMonth",
+                table: "MembershipTypes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "MembershipTypes",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClassesPerMonth",
+                table: "MembershipTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "MembershipTypes");
+        }
+    }
+}
