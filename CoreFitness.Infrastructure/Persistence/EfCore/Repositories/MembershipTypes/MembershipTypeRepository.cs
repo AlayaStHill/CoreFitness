72dfeac6eb6cc35a703c80701f58ee7209d83886
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-04-01 08:34:03 +0200

    feat(membership): add repository implementation for fetching featured membershipTypes

---
+++ CoreFitness.Infrastructure/Persistence/EfCore/Repositories/MembershipTypes/MembershipTypeRepository.cs
@@ -0,0 +1,29 @@
+﻿using CoreFitness.Application.MembershipTypes;
+using CoreFitness.Application.MembershipTypes.Outputs;
+using CoreFitness.Infrastructure.Persistence.EfCore.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreFitness.Infrastructure.Persistence.EfCore.Repositories.MembershipTypes;
+
+public sealed class MembershipTypeRepository(PersistenceContext context) : IMembershipTypeRepository
+{
+    public async Task<IReadOnlyCollection<MembershipTypeFeaturedOutput>> GetFeaturedAsync(CancellationToken ct)
+    {
+        return await context.MembershipTypes
+            .AsNoTracking()
+            .Where(membershipType => membershipType.IsFeatured)
+            .OrderBy(membershipType => membershipType.PricePerMonth)
+            .Select(membershipType => new MembershipTypeFeaturedOutput
+            {
+                Id = membershipType.Id.Value,
+                Name = membershipType.Name,
+                Description = membershipType.Description,
+                ClassesPerMonth = membershipType.ClassesPerMonth,
+                PricePerMonth = membershipType.PricePerMonth,
+                Benefits = membershipType.Benefits
+                    .Select(benefit => benefit.Text)
+                    .ToArray()
+            })
+            .ToListAsync(ct);
+    }
+}
--- CoreFitness.Presentation.WebApp/Controllers/Public/MembershipsController.cs
+++ CoreFitness.Presentation.WebApp/Controllers/Public/MembershipsController.cs
@@ -1,23 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreFitness.Application.MembershipTypes;
+using CoreFitness.Presentation.WebApp.Models.Memberships;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreFitness.Presentation.WebApp.Controllers.Public;
 
-public class MembershipsController : Controller
+public class MembershipsController(IMembershipTypeService memberTypeService) : Controller
 {
     public async Task<IActionResult> Index()
     {
-        //var result = await memberTypeService.GetFeaturedMembershipTypesAsync();
-        //if (result.IsFailure)
-        //{
-        //    return View("Error");
-        //}
+        var result = await memberTypeService.GetFeaturedMembershipTypesAsync();
+        if (result.IsFailure)
+        {
+            return View("Error");
+        }
 
-        //var viewModel = new MembershipsIndexViewModel()
-        //{
-        //    Memberships = result.Value
-        //};
-        //return View(viewModel);
-
-        return View();
+        var viewModel = new MembershipsIndexViewModel()
+        {
+            Memberships = result.Value
+        };
+        return View(viewModel);
     }
 }
