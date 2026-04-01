5bef4f9e15620e7ebb17a72195c0b87ae92a56be
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-04-01 08:10:48 +0200

    feat(membership): add css and html structure för Our Memberships page

--- CoreFitness.Presentation.WebApp/Controllers/Public/MembershipsController.cs
+++ CoreFitness.Presentation.WebApp/Controllers/Public/MembershipsController.cs
@@ -1,23 +1,23 @@
-﻿using CoreFitness.Application.MembershipTypes;
-using CoreFitness.Presentation.WebApp.Models.Memberships;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace CoreFitness.Presentation.WebApp.Controllers.Public;
 
-public class MembershipsController(IMembershipTypeService memberTypeService) : Controller
+public class MembershipsController : Controller
 {
     public async Task<IActionResult> Index()
     {
-        var result = await memberTypeService.GetFeaturedMembershipTypesAsync();
-        if (result.IsFailure)
-        {
-            return View("Error");
-        }
+        //var result = await memberTypeService.GetFeaturedMembershipTypesAsync();
+        //if (result.IsFailure)
+        //{
+        //    return View("Error");
+        //}
 
-        var viewModel = new MembershipsIndexViewModel()
-        {
-            Memberships = result.Value
-        };
-        return View(viewModel);
+        //var viewModel = new MembershipsIndexViewModel()
+        //{
+        //    Memberships = result.Value
+        //};
+        //return View(viewModel);
+
+        return View();
     }
 }
