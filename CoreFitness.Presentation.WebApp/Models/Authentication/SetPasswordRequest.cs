edafcf41ebe6485c507f98f5bb610e04d2219938
AlayaStHill <alaya.st.hill@gmail.com>
2026-04-02 13:25:53 +0200

    feat(identity-register): Add set password form markup.

--- CoreFitness.Presentation.WebApp/Models/Authentication/SetPasswordRequest.cs
+++ CoreFitness.Presentation.WebApp/Models/Authentication/SetPasswordRequest.cs
@@ -10,16 +10,17 @@ public class SetPasswordRequest
      ErrorMessage = "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character"
  )]
     [DataType(DataType.Password)]
-    [Display(Name = "Password", Prompt = "Enter Password")]
+    [Display(Name = "Password *", Prompt = "Enter Password")]
     public string Password { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Password must be confirmed")]
     [Compare(nameof(Password), ErrorMessage = "Password must match")]
     [DataType(DataType.Password)]
-    [Display(Name = "Confirm Password", Prompt = "Confirm Password")]
+    [Display(Name = "Confirm Password *", Prompt = "Confirm Password")]
     public string ConfirmPassword { get; set; } = string.Empty;
 
     [Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the terms and conditions")]
+    [Display(Name = "Accept user terms and conditions")]
     public bool TermsAndConditions { get; set; }
 }
 
