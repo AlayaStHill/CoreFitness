b265e9e2bf780e0ac07b98e7e04af2eeafb6a22e
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-04-04 18:36:33 +0200

    feat(my-account): static view implemented

---
+++ CoreFitness.Presentation.WebApp/Controllers/Account/MyAccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFitness.Presentation.WebApp.Controllers.Account;
+
+public class MyAccountController : Controller
+{
+    public IActionResult AboutMe()
+    {
+        return View();
+    }
+}
