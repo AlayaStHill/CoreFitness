1e81948fd515580300adb7a05c72dd0200abe325
AlayaStHill <alaya.st.hill@gmail.com>
2026-04-10 13:43:57 +0200

    feat(error): Add ErrorController with status code handling.

---
+++ CoreFitness.Presentation.WebApp/Controllers/Public/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFitness.Presentation.WebApp.Controllers.Public;
+
+public class ErrorController : Controller
+{
+    [Route("error/404")]
+    public IActionResult NotFoundPage()
+    {
+        Response.StatusCode = 404;
+        return View("NotFound");
+    }
+}
