27ebf1b7b78c7882e509eb06c3d77f13808f59ba
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-04-15 11:35:22 +0200

    feat(create-session-design): add date time picker and some css clean up

--- CoreFitness.Presentation.WebApp/Models/Admin/CreateWorkoutSessionRequest.cs
+++ CoreFitness.Presentation.WebApp/Models/Admin/CreateWorkoutSessionRequest.cs
@@ -6,33 +6,47 @@ namespace CoreFitness.Presentation.WebApp.Models.Admin;
 public class CreateWorkoutSessionRequest : IValidatableObject
 {
     [Required(ErrorMessage = "Workout type is required.")]
-    public Guid WorkoutTypeId { get; set; }
+    public Guid? WorkoutTypeId { get; set; }
 
     [Required(ErrorMessage = "Date is required.")]
-    public DateOnly Date { get; set; }
+    public DateOnly? Date { get; set; }
 
     [Required(ErrorMessage = "Start time is required.")]
-    public TimeOnly StartTime { get; set; }
+    public TimeOnly? StartTime { get; set; }
 
     [Required(ErrorMessage = "Duration is required.")]
-    public TimeSpan Duration { get; set; }
+    public TimeSpan? Duration { get; set; }
 
+    [Required(ErrorMessage = "Capacity is required.")]
     [Range(1, 40, ErrorMessage = "Capacity must be between {1} and {2}.")]
-    public int Capacity { get; set; }
+    public int? Capacity { get; set; }
 
     // för att visa dropdown-lista över tillgängliga workouttypes <select>
     public IEnumerable<SelectListItem> WorkoutTypes { get; set; } = [];
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
-        if (Duration <= TimeSpan.Zero)
+        if (Duration.HasValue && Duration.Value <= TimeSpan.Zero)
         {
             yield return new ValidationResult(
                 "Duration must be greater than 00:00.",
                 [nameof(Duration)]);
         }
 
-        DateTime localDateTime = Date.ToDateTime(StartTime);
+        if (!Date.HasValue || !StartTime.HasValue)
+        {
+            yield break;
+        }
+
+        DateTime localDateTime = Date.Value.ToDateTime(StartTime.Value);
+
+        if (localDateTime < DateTime.Now)
+        {
+            yield return new ValidationResult(
+                "Start date and time cannot be in the past.",
+                [nameof(Date), nameof(StartTime)]);
+        }
+
         DateTime minSafeLocal = DateTimeOffset.MinValue.UtcDateTime.AddHours(14);
         DateTime maxSafeLocal = DateTimeOffset.MaxValue.UtcDateTime.AddHours(-14);
 
